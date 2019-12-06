c6aa2fa1f45a6f2293a1ed45c0dfad0ebf6b39f6
ravi <rtanwar616@gmail.com>
2019-12-06 22:22:17 +0530

    one more try

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import linear_kernel
 
-df1=pd.read_csv('tmdb_5000_credits.csv')
+# df1=pd.read_csv('tmdb_5000_credits.csv')
 df2=pd.read_csv('tmdb_5000_movies.csv')
 
-df1.columns = ['id','tittle','cast','crew']
-df2= df2.merge(df1,on='id')
+# df1.columns = ['id','title','cast','crew']
+# df2= df2.merge(df1,on='id')
 
 C= df2['vote_average'].mean()
 m= df2['vote_count'].quantile(0.9)
@@ -54,23 +54,27 @@ indices = pd.Series(df2.index, index=df2['title']).drop_duplicates()
 
 # Function that takes in movie title as input and outputs most similar movies
 def get_recommendations(title, cosine_sim=cosine_sim):
-    # Get the index of the movie that matches the title
-    idx = indices[title]
+    if title not in df2['title'].values:
+        return ('This movie is not in our database.\nPlease check if you spelled it correct using camel casing')
+    else:
+        # Get the index of the movie that matches the title
+        # if title not in df2
+        idx = indices[title]
 
-    # Get the pairwsie similarity scores of all movies with that movie
-    sim_scores = list(enumerate(cosine_sim[idx]))
+        # Get the pairwsie similarity scores of all movies with that movie
+        sim_scores = list(enumerate(cosine_sim[idx]))
 
-    # Sort the movies based on the similarity scores
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        # Sort the movies based on the similarity scores
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
-    # Get the scores of the 10 most similar movies
-    sim_scores = sim_scores[1:11]
+        # Get the scores of the 10 most similar movies
+        sim_scores = sim_scores[1:11]
 
-    # Get the movie indices
-    movie_indices = [i[0] for i in sim_scores]
+        # Get the movie indices
+        movie_indices = [i[0] for i in sim_scores]
 
-    # Return the top 10 most similar movies
-    return df2['title'].iloc[movie_indices]
+        # Return the top 10 most similar movies
+        return df2['title'].iloc[movie_indices]
 
 app = Flask(__name__)
 
